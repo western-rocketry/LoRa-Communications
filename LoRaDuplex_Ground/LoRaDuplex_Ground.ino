5eb268bf34658e141fff241ecc0631a5695709e5
Winston-Lu <wlu2268@gmail.com>
2020-03-08 11:40:19 -0400

    fixed formatting consistency

--- LoRaDuplex_Ground/LoRaDuplex_Ground.ino
+++ LoRaDuplex_Ground/LoRaDuplex_Ground.ino
@@ -122,20 +122,20 @@ void onReceive(int packetSize){
       if(byte((gyx+gyy+gyz)%256)!=byte(chk1)) Serial.println(F("CS FAILED - ACCELEROMETER"));
       //GPS
       String gpsConnected = fix ? "True" : "False"; 
-      Serial.println("Connection: "+gpsConnected+
-                   "\nSatellites: "+String(num)+
-                   "\nFails: " + String(fail)+
-                   "\nAltitude: " + String(alt.num));  
+      Serial.println("Connection "+gpsConnected+
+                   "\nSatellites "+String(num)+
+                   "\nFails " + String(fail)+
+                   "\nAltitude " + String(alt.num));  
       if(byte((msg[16]+msg[17]+msg[18]+msg[19]+msg[20]+msg[21]+msg[22])%256)!=byte(chk2)) Serial.println(F("CS FAILED - GPS ALTITUDE/CONNECTION")); 
-      Serial.println("Latitude: "+String(lat.num)+
-                  "\nLongitude: "+String(lon.num));
+      Serial.println("Latitude "+String(lat.num)+
+                  "\nLongitude "+String(lon.num));
       if(byte((msg[24]+msg[25]+msg[26]+msg[27]+msg[28]+msg[29]+msg[30]+msg[31])%256)!=byte(chk3)) Serial.println(F("CS FAILED - GPS COORDINATES"));   
-      Serial.println("Speed (Knots): "+String(knots.num)+
-                   "\nAngle from N: "+String(angle.num));           
+      Serial.println("Speed (Knots) "+String(knots.num)+
+                   "\nAngle from N "+String(angle.num));           
       if(byte((msg[33]+msg[34]+msg[35]+msg[36]+msg[37]+msg[38]+msg[39]+msg[40])%256)!=byte(chk4)) Serial.println(F("CS FAILED - SPEED/ANGLE")); 
-      Serial.println("Vertical DoP: "+String(hdop.num)+
-                 "\nHorizontal DoP: "+String(vdop.num)+
-                 "\nPositional DoP: "+String(pdop.num));
+      Serial.println("Vertical DoP "+String(hdop.num)+
+                 "\nHorizontal DoP "+String(vdop.num)+
+                 "\nPositional DoP "+String(pdop.num));
       if(byte((msg[42]+msg[43]+msg[44]+msg[45]+msg[46]+msg[47]+msg[48]+msg[49]+msg[50]+msg[51]+msg[52]+msg[53])%256)!=byte(chk5)) Serial.println(F("CS FAILED - DILUTION OF PERCISION")); 
       Serial.println("Temp1: "+String(temp1.num));             
 
